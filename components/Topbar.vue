6c9fbbccba17e2e4a5b16e2ee70f0519cd1471ab
rafelivorra <rafel.ivorra@syr.es>
2024-03-13 10:10:22 +0100

    change styles in Topbar.vue

--- components/Topbar.vue
+++ components/Topbar.vue
@@ -1,23 +1,20 @@
 <template>
   <div class="Topbar">
-    <NuxtLink to="/" class="logoImg"><img class="logo" src="/logo.png" /></NuxtLink>
-    
-    <div class="usuario-options">
-      <b-button class="refresh-button" @click="refreshPage()">
+    <div class="top-content">
+      <NuxtLink to="/" class="logoImg"><img class="logo" src="/logo.png" /></NuxtLink>
+      <div class="usuario-options">
+        <b-button class="refresh-button" @click="refreshPage()">
           <b-icon icon="arrow-repeat"></b-icon>
-          <small>{{$config.clientVersion}}</small>
-      </b-button>
-      <p class="m-0">{{ this.$store.state.User }}</p>
-      <svg
-        @click="logOut()"
-        class="icono-rojo"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 512 512"
-      >
-        <path
-          d="M160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96C43 32 0 75 0 128V384c0 53 43 96 96 96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H96c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32h64zM504.5 273.4c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22v72H192c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32H320v72c0 9.6 5.7 18.2 14.5 22s19 2 26-4.6l144-136z"
-        />
-      </svg>
+          <small>{{ $config.clientVersion }}</small>
+        </b-button>
+        <svg @click="logOut()" class="icono-rojo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path
+            d="M160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96C43 32 0 75 0 128V384c0 53 43 96 96 96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H96c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32h64zM504.5 273.4c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22v72H192c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32H320v72c0 9.6 5.7 18.2 14.5 22s19 2 26-4.6l144-136z" />
+        </svg>
+      </div>
+    </div>
+    <div class="usuario-name-container">
+      <p class="usuario-name">{{ this.$store.state.User }}</p>
     </div>
   </div>
 </template>
@@ -34,7 +31,7 @@ export default {
         showDenyButton: true,
         confirmButtonText: "Salir",
         confirmButtonColor: "#000",
-        
+
       }).then((result) => {
         if (result.isConfirmed) {
           this.$axios.post("/api/usuarios/logout", {
@@ -49,7 +46,7 @@ export default {
         }
       });
     },
-    refreshPage: function() {
+    refreshPage: function () {
       window.location.href = window.location.href
     }
   },
@@ -59,41 +56,63 @@ export default {
 <style scoped>
 .Topbar {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: space-between;
   max-width: 425px;
+  margin-top: 1rem;
 }
-.Topbar > * {
-  margin: 2rem;
+
+.top-content {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 }
-.logoImg{
+
+.logoImg {
   width: 40%;
   margin-left: 1rem !important;
 }
 
-
-
 .usuario-options {
   display: flex;
   align-items: center;
-  grid-gap: 1.3rem;
-  gap: 1.2rem;
   justify-content: flex-end;
 }
+
+.usuario-name-container {
+  width: 100%; 
+  display: flex;
+  justify-content: flex-end;
+}
+
+.usuario-name {
+  font-size: 0.8rem;
+  color: darkgray;
+  text-align: right;
+  margin-right: 0.3rem;
+}
+
 .icono-rojo {
   width: 24px;
   height: 24px;
   fill: red;
+  margin-right: 0.3rem;
 }
+
 .logo {
   width: 200px;
 }
-.swal2-title{ font-size: 1.25rem ! important; }
+
+.swal2-title {
+  font-size: 1.25rem ! important;
+}
 
 .refresh-button {
   background-color: transparent;
-  border:none;
+  border: none;
   color: rgb(123, 123, 123);
   display: flex;
   align-items: center;
@@ -103,15 +122,18 @@ export default {
 .refresh-button small {
   padding: 0 4%;
 }
+
 @media (max-width: 375px) {
-  .logo{
-    width:150px;
+  .logo {
+    width: 150px;
   }
-  .logoImg{
+
+  .logoImg {
     width: 30%;
   }
+
   .usuario-options {
-    gap:0.8rem;
+    gap: 0.8rem;
   }
 }
 </style>
